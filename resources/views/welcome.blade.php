9a3186542afcbfe3d9c5d612efbf9ce1e6ba67d0
Ambrose Bako <bakoambrose@gmail.com>
2020-03-25 18:32:23 +0000

    Modify DB credentials

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,9 +17,12 @@
     
         <!-- Three columns -->
         <div class="flex mb-4">
-            <div class="w-1/3 bg-gray-400 h-12"></div>
-            <div class="w-1/3 bg-gray-500 h-12"></div>
-            <div class="w-1/3 bg-gray-400 h-12"></div>
+            <div class="w-1/3 h-12"></div>
+            
+            <div class="w-1/3 bg-gray-500 h-12 mt-40">
+            
+            </div>
+            <div class="w-1/3 h-12"></div>
         </div>
   
             
